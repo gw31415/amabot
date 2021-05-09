c6ccdfbfc5cc46f429940185478473bfdc92832d
Amadeus_vn <git@amas.dev>
2021-05-09 18:48:25 +0900

    add pingpong

---
+++ libamabot/pingpong.go
@@ -0,0 +1,27 @@
+package libamabot
+
+import (
+	"github.com/bwmarrin/discordgo"
+)
+
+func init() {
+	handlers_db = append(handlers_db, handler{
+		id:   "pingpong",
+		help: "catch ping and reply pong",
+		main: func(s *discordgo.Session, m *discordgo.MessageCreate) {
+			if m.Author.ID == s.State.User.ID {
+				return
+			}
+			if m.Author.Bot {
+				return
+			}
+			if m.Content == "ping" {
+				s.ChannelMessageSend(m.ChannelID, "Pong!")
+			}
+
+			if m.Content == "pong" {
+				s.ChannelMessageSend(m.ChannelID, "Ping!")
+			}
+		},
+	})
+}
