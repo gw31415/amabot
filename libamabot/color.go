5383916b5d97bb4c8d3f6479cb0098f9fc913ebd
Amadeus_vn <git@amas.dev>
2021-05-10 09:25:24 +0900

    add color

--- libamabot/color.go
+++ libamabot/color.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"image/color"
 	"log"
+	"strconv"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
@@ -45,11 +46,34 @@ func init() {
 						color := colornames.Map[name]
 						s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
 							Title:       name,
-							Description: fmt.Sprintf("ColorName: %s\nColorCode: #%02x%02x%02x", name, color.R, color.G, color.B),
+							Description: fmt.Sprintf("ColorName: `%s`\nColorCode: `#%02x%02x%02x`", name, color.R, color.G, color.B),
 							Color:       rgbaToInt(color),
 						})
 						return
 					}
+				}
+				if len(arg) == 7 && arg[0] == '#' {
+					c_s := arg[1:]
+					c_i, err := strconv.ParseInt(c_s, 16, 0)
+					if err != nil {
+						panic(err)
+					}
+					if 0 > c_i || c_i > 0xffffff {
+						panic("overflow")
+					}
+					name := "(none)"
+					for _, color_index := range colornames.Names {
+						if rgbaToInt(colornames.Map[color_index]) == int(c_i) {
+							name = color_index
+							break
+						}
+					}
+					s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
+						Title:       arg,
+						Description: fmt.Sprintf("ColorName: `%s`\nColorCode: `%s`", name, arg),
+						Color:       int(c_i),
+					})
+					return
 				}
 				panic("color not found: " + arg)
 			}
