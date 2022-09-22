bc2b41cdb38111fa9f81557995d18f16b5905afd
gw31415 <git@amas.dev>
2022-09-23 03:12:07 +0900

    changed to slash-command color.go

--- libamabot/color.go
+++ libamabot/color.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"image/color"
 	"strconv"
-	"strings"
 
 	"github.com/bwmarrin/discordgo"
 	"golang.org/x/image/colornames"
@@ -16,19 +15,44 @@ func rgbaToInt(c color.RGBA) int {
 }
 
 func init() {
-	messageCmd(func(ctx context.Context, opts AmabotOptions, s *discordgo.Session, m *discordgo.MessageCreate) {
-		prefix := opts.GetMessageCmdPrefix()
-		cmd := ctx.Value("cmd").(string)
-		arg := strings.TrimSpace(m.Content[len(prefix)+len(cmd):])
+	slashCmd(&discordgo.ApplicationCommand{
+		Name:        "color",
+		Description: "Get color information",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "color",
+				Description: "Color name or code",
+				Required:    true,
+			},
+		},
+	}, func(ctx context.Context, opts AmabotOptions, s *discordgo.Session, i *discordgo.InteractionCreate) {
+		options := i.ApplicationCommandData().Options
+		optionMap := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(options))
+		for _, opt := range options {
+			optionMap[opt.Name] = opt
+		}
+		arg := optionMap["color"].StringValue()
 		for _, name := range colornames.Names {
 			if arg == name {
 				color := colornames.Map[name]
-				s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
-					Title:       name,
-					Description: fmt.Sprintf("ColorName: `%s`\nColorCode: `#%02x%02x%02x`", name, color.R, color.G, color.B),
-					Color:       rgbaToInt(color),
+				e := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Embeds: []*discordgo.MessageEmbed{
+							{
+								Title:       name,
+								Description: fmt.Sprintf("ColorName: `%s`\nColorCode: `#%02x%02x%02x`", name, color.R, color.G, color.B),
+								Color:       rgbaToInt(color),
+							},
+						},
+					},
 				})
-				return
+				if e != nil {
+					panic(e)
+				} else {
+					return
+				}
 			}
 		}
 		if len(arg) == 7 && arg[0] == '#' {
@@ -47,12 +71,23 @@ func init() {
 					break
 				}
 			}
-			s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
-				Title:       arg,
-				Description: fmt.Sprintf("ColorName: `%s`\nColorCode: `%s`", name, arg),
-				Color:       int(c_i),
+			e := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Embeds: []*discordgo.MessageEmbed{
+						{
+							Title:       arg,
+							Description: fmt.Sprintf("ColorName: `%s`\nColorCode: `%s`", name, arg),
+							Color:       int(c_i),
+						},
+					},
+				},
 			})
-			return
+			if e != nil {
+				panic(e)
+			} else {
+				return
+			}
 		}
 		panic("color not found: " + arg)
 	})
